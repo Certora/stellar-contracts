12fd8a1d0ac3b618aff4fef202b4234be9c212bd
Chandrakana Nandi <cnandi@cs.washington.edu>
2026-03-03 09:14:30 -0800

    actually push the code this time

---
+++ packages/access/src/access_control/specs/access_control_contract.rs
@@ -0,0 +1,37 @@
+use soroban_sdk::{contract, contractimpl, Address, Env};
+use stellar_macros::{has_any_role, has_role, only_admin, only_any_role, only_role};
+
+use crate as stellar_access;
+use crate::access_control::{AccessControl, *};
+
+#[contract]
+pub struct AccessControlContract;
+
+#[contractimpl]
+impl AccessControlContract {
+    pub fn access_control_constructor(e: &Env, admin: Address) {
+        set_admin(e, &admin);
+    }
+
+    #[only_admin]
+    pub fn admin_function(e: &Env) {}
+
+    #[has_role(caller, "role1")]
+    pub fn role1_func(e: &Env, caller: Address) {}
+
+    #[only_role(caller, "role1")]
+    pub fn role1_auth_func(e: &Env, caller: Address) {}
+
+    #[has_any_role(caller, ["role1", "role2"])]
+    pub fn role1_or_role2_func(e: &Env, caller: Address) {}
+
+    #[only_any_role(caller, ["role1", "role2"])]
+    pub fn role1_or_role2_auth_func(e: &Env, caller: Address) {}
+
+    #[has_role(caller1, "role1")]
+    #[has_role(caller2, "role2")]
+    pub fn role1_and_role2_func(e: &Env, caller1: Address, caller2: Address) {}
+}
+
+#[contractimpl(contracttrait)]
+impl AccessControl for AccessControlContract {}
---
+++ packages/access/src/access_control/specs/helper.rs
@@ -0,0 +1,37 @@
+use cvlr::clog;
+use cvlr_soroban::SorobanAsCvlr;
+use soroban_sdk::{Address, Env, Symbol};
+
+use crate::access_control::{storage::RoleAccountKey, AccessControlStorageKey};
+
+/// Returns `Some(Address)` if a pending owner is set, or `None` if there is no
+/// pending ownership transfer.
+///
+/// # Arguments
+///
+/// * `e` - Access to the Soroban environment.
+pub fn get_pending_admin(e: &Env) -> Option<Address> {
+    let pending_admin =
+        e.storage().temporary().get::<_, Address>(&AccessControlStorageKey::PendingAdmin);
+    if let Some(pending_admin_internal) = pending_admin.clone() {
+        clog!(pending_admin_internal.as_cvlr());
+    }
+    pending_admin
+}
+
+/// Returns `Some(Address)` if an account exists at the specified index for the
+/// given role, or `None` if there is no account at that index.
+///
+/// # Arguments
+///
+/// * `e` - Access to the Soroban environment.
+/// * `role` - The role to query.
+/// * `index` - The index of the account to retrieve.
+pub fn get_role_account(e: &Env, role: &Symbol, index: u32) -> Option<Address> {
+    let key = AccessControlStorageKey::RoleAccounts(RoleAccountKey { role: role.clone(), index });
+    let account = e.storage().persistent().get::<_, Address>(&key);
+    if let Some(account_internal) = account.clone() {
+        clog!(account_internal.as_cvlr());
+    }
+    account
+}
